34fb25caf2a7b14310647a9f7ee1e25faef7bf48
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2023-03-10 20:41:21 -0300

    Update

---
+++ eclipseJava/estruturaDeDados/src/estruturaDeDados/Main.java
@@ -0,0 +1,33 @@
+package estruturaDeDados;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/* Primeira implementaçãoi de um vetor
+		
+		int vetor[]; // declaração do vetor
+		
+		vetor = new int[10]; // alocação de 10 posições no vetor*/
+		
+		int [] vetor = new int[10]; // combinação
+		
+		double [] salarios = new double[10];
+		
+		String [] meses = new String[10];
+		
+		int tamanho = 10; // tamanho do vetor
+		int [] v = new int [tamanho];
+		
+		int i; // indice
+		
+		for (i = 0; i < tamanho; i++) {
+			v[i] = i;
+		}
+		
+		
+		
+		
+
+	}
+
+}
