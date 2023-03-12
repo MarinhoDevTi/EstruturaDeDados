62794d44d064070b84ae7a182d2e5e7d573e3fbf
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2023-03-12 16:13:01 -0300

    Update Main.java

--- estruturaDeDados/src/estruturaDeDados/Main.java
+++ estruturaDeDados/src/estruturaDeDados/Main.java
@@ -3,9 +3,32 @@ package estruturaDeDados;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* Primeira implementaçãoi de um vetor
 		
-		System.out.println("Bom Dia");
+		int vetor[]; // declaração do vetor
+		
+		vetor = new int[10]; // alocação de 10 posições no vetor*/
+		
+		int [] vetor = new int[10]; // combinação
+		
+		double [] salarios = new double[10];
+		
+		String [] meses = new String[10];
+		
+		int tamanho = 10; // tamanho do vetor
+		int [] v = new int [tamanho];
+		
+		int i; // indice
+		
+		for (i = 0; i < tamanho; i++) {
+			v[i] = i;
+			System.out.println("v[" + i + "]" + " = " +v[i]); 
+			System.out.println("Olá");
+		}
+		
+		
+		
+		
 
 	}
 
